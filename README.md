dc4aa0d20feb03d71dfc9eb6dc5b9da1c4bbd1ee
yuliaslatvitskaya <yuli.slat@yandex.ru>
2026-02-06 15:16:08 +0100

    feat: add new style for card and delete yarn

--- README.md
+++ README.md
@@ -2,23 +2,19 @@
 
 ## Project setup
 ```
-yarn install
+npm install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-yarn serve
+npm serve
 ```
 
 ### Compiles and minifies for production
 ```
-yarn build
+npm build
 ```
 
-### Lints and fixes files
-```
-yarn lint
-```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+### Author: Yulia Slatvitskaya 
+https://github.com/matrasjan
--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,17 +1,24 @@
 <template>
   <el-card class="weather-card" v-if="weather">
-    <h2 class="weather-title">{{ weather.name }}</h2>
-    <img
-        v-if="weather.weather?.length"
-        :src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`"
-        :alt="weather.weather[0].description"
-    />
-    <p v-if="weather.weather?.length" class="weather-desc">
-      {{ weather.weather[0].description }}
-    </p>
-    <p>🌡 {{ Math.round(weather.main.temp) }} °C</p>
-    <p>💧 {{ Math.round(weather.main.humidity) }} %</p>
-    <p>🌬 {{ Math.round(weather.wind.speed) }} м/с</p>
+    <div class="card-content">
+      <div class="left-block">
+        <h2 class="weather-title">{{ weather.name }}</h2>
+        <img
+            v-if="weather.weather?.length"
+            :src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`"
+            :alt="weather.weather[0].description"
+        />
+      </div>
+
+      <div class="right-block">
+        <p v-if="weather.weather?.length" class="weather-desc">
+          {{ weather.weather[0].description }}
+        </p>
+        <p class="degrees">🌡 {{ Math.round(weather.main.temp) }} °C</p>
+        <p class="degrees">💧 {{ Math.round(weather.main.humidity) }} %</p>
+        <p class="degrees">🌬 {{ Math.round(weather.wind.speed) }} м/с</p>
+      </div>
+    </div>
   </el-card>
 </template>
 
@@ -28,10 +35,35 @@ const props = defineProps<{ weather: WeatherResponse | null }>()
   padding: 16px;
 }
 
+.card-content {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.left-block {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.right-block {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
 .weather-title {
   color: #040a42;
   font-size: 30px;
   font-weight: 600;
+  margin-bottom: 8px;
+  text-align: center;
+
+}
+
+.degrees {
+  font-size: 18px;
 }
 
 .weather-desc {
@@ -39,5 +71,21 @@ const props = defineProps<{ weather: WeatherResponse | null }>()
   font-weight: 600;
   color: #040a42;
   text-transform: capitalize;
+}
+
+@media (max-width: 440px) {
+  .card-content {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .right-block {
+    width: 100%;
+    text-align: center;
+  }
+
+  .left-block img {
+    margin-bottom: 16px;
+  }
 }
 </style>
